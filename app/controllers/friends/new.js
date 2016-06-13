1960e4db4a466ad8ea0dbe408301d362c10ea40d
Phillip Lackey <dp078971@MBP-C02NJ4G4G3QN.local>
2016-06-12 22:27:31 -0500

    added a friends show view and logic to save new friends in controller

--- app/controllers/friends/new.js
+++ app/controllers/friends/new.js
@@ -3,7 +3,8 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     save(model) {
-      console.log('saving model in controller')
+      console.log('saving model in controller');
+      this.transitionToRoute('friends.show', model);
     },
     cancel() {
       console.log('canceling in controller')
