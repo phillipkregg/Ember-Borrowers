3156e977b3280ebb2e5bd6b784b59c28cf4b9adf
Phillip Lackey <dp078971@MBP-C02NJ4G4G3QN.local>
2016-06-13 22:21:52 -0500

    Added editing capability to friends

--- app/components/friends/edit-form.js
+++ app/components/friends/edit-form.js
@@ -32,7 +32,7 @@ export default Ember.Component.extend({
     },
     cancel() {
       console.log('cancel action in component');
-      this.cancel();
+      this.cancel(this.get('model'));
     }
 
   }
